058379662e257089dc2609b308e8ba33768d4d03
wzz <13731160065@users.noreply.github.com>
2017-12-08 05:00:08 +0800

    添加项目展开

---
+++ index/test1/script.js
@@ -0,0 +1,8 @@
+function back() {
+    OCH5_dismissViewController();
+}
+
+function btn() {
+	var str = "wzzoch5://test2/test2.html";
+    OCH5_presentVCWithUrl(str);
+}
---
+++ index/test2/script.js
@@ -0,0 +1,7 @@
+function back() {
+    OCH5_dismissViewController();
+}
+
+function btn() {
+    window.alert("测试, 1.0.1");
+}
